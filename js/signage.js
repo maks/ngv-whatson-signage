9e519e3664b3a66f78da76d0784e446d06a3983e
Maksim Lin <maks@manichord.com>
2012-06-05 16:42:29 +1000

    added jquery and sample xml data.

--- js/signage.js
+++ js/signage.js
@@ -1,2 +1,11 @@
+jQuery(init);
+
 function init() {
+    console.log("init");
+
+    jQuery.get("../docs/ngvi-sample.xml", null, gotXml, 'xml');
+}
+
+function gotXml(data) {
+    console.log('xml data:', data);
 }
