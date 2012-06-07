01f0df1d48449bb6f205a6e477ae5a3f75b78c38
Maksim Lin <maks@manichord.com>
2012-06-07 16:59:37 +1000

    more templating

--- js/signage.js
+++ js/signage.js
@@ -45,11 +45,28 @@ function gotXml(data) {
     console.log('xml data:', data);
     $(data).find("program").each( function() {
         var prog = { 
-            title : $(this).find("title").text() 
+            title : $(this).find("title").text(),
+            time: $(this).find("start_time").text(),
+            venue: $(this).find("venue").text()
         };
         console.log('program:'+ prog.title);
-        var output = Mustache.render("<li>{{title}}</li>", prog);
-        $("#programs").append(output);
+        var output = Mustache.render(
+            "<li>{{time}} {{title}} <br/> {{venue}}</li>",
+            prog);
+        $("#program_list").append(output);
+    });
+    
+    $(data).find("tour").each( function() {
+        var prog = { 
+            title : $(this).find("title").text(),
+            time: $(this).find("start_time").text(),
+            venue: $(this).find("venue").text()
+        };
+        console.log('program:'+ prog.title);
+        var output = Mustache.render(
+            "<li>{{time}} {{title}} <br/> {{venue}}</li>",
+            prog);
+        $("#tour_list").append(output);
     });
     
 }
