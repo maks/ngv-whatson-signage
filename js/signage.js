1b9677b5598eea7371225efb0d3e02878fcf1453
Maksim Lin <maks@manichord.com>
2012-06-07 16:26:45 +1000

    basic skeleton app fetching xml & basic rendering
    Can now fetch sample xml, extract program title and render into basic
    mustache template and then insert into page DOM.

--- js/signage.js
+++ js/signage.js
@@ -1,11 +1,55 @@
-jQuery(init);
+/*jshint browser: true, jquery:true, devel: true, undef:true */
+/*global Mustache: true */
+
+
+var dataRequestPending = false,
+    playList = null;
+
+
+$(init);
 
 function init() {
-    console.log("init");
+    var TICK = 1 * 1000;
+    
+    console.log("starting ticker...");
 
-    jQuery.get("../docs/ngvi-sample.xml", null, gotXml, 'xml');
+    setInterval(mainLoop, TICK);
+}
+
+/**
+ * ye olde traditional mainloop - in this case called once per TICK.
+ */
+function mainLoop() {
+    //console.log("tick");    
+    checkForData();
+}
+
+
+function checkForData() {
+    var DATA_URL = "docs/ngvi-sample.xml";
+    if (dataRequestPending) {
+        return;
+    }
+    
+    if (playList === null ) {
+        dataRequestPending = true;
+        console.log("fetching "+DATA_URL);
+        $.get(DATA_URL, null, gotXml, 'xml');
+    }    
 }
 
 function gotXml(data) {
+    dataRequestPending = false;
+    playList = {}; //TODO
+        
     console.log('xml data:', data);
+    $(data).find("program").each( function() {
+        var prog = { 
+            title : $(this).find("title").text() 
+        };
+        console.log('program:'+ prog.title);
+        var output = Mustache.render("<li>{{title}}</li>", prog);
+        $("#programs").append(output);
+    });
+    
 }
